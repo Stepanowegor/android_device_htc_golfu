0cfb62b8213a0c15745af437327a0df86affc3db
munjeni <sharecard17@gmail.com>
2013-04-14 16:22:07 +0200

    update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo generic_golfu-userdebug
-add_lunch_combo generic_golfu-eng
+add_lunch_combo cm_golfu-eng
 add_lunch_combo cm_golfu-userdebug
